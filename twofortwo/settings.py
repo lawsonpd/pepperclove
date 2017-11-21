79a94b161cc931455bde443e162b4df31f86a4dc
Peter <lawsonpd@gmail.com>
2017-11-20 20:15:15 -0600

    Remove 'name' property from database config in settings.

--- twofortwo/settings.py
+++ twofortwo/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+import dj_database_url
+
 import raven
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -90,11 +92,10 @@ DATABASES = {
     # },
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.path.join(BASE_DIR, 'db.postgresql'),
+        # 'NAME': os.path.join(BASE_DIR, 'db.postgresql'),
     },
 }
 
-import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
