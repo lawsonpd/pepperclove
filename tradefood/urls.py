8c69a083a6b7bc959415892fa12f579716eb3e99
Peter <lawsonpd@gmail.com>
2017-11-23 21:06:33 -0500

    Remove registration endpoint.

--- tradefood/urls.py
+++ tradefood/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  url(r'^register/$', views.register),
-  # url(r'^register/$', views.temp_register_unavailable),
+  # url(r'^register/$', views.register),
+  url(r'^register/$', views.temp_register_unavailable),
   url(r'^login/$', views.login_view),
   url(r'^logout/$', views.logout_view),
   url(r'^$', views.home),
