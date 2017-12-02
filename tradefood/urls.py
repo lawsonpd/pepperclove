a9ac0004038d79c22360e0dcb5730fda9f401616
Peter <lawsonpd@gmail.com>
2017-12-02 09:16:02 -0600

    Use reverse url resolution in all templates. Add name to /register url.

--- tradefood/urls.py
+++ tradefood/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  # url(r'^register/$', views.register),
+  # url(r'^register/$', views.register, name='register'),
   url(r'^register/$', views.temp_register_unavailable, name='register-unavail'),
   url(r'^login/$', views.login_view, name='login'),
   url(r'^logout/$', views.logout_view, name='logout'),
