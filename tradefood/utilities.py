6d989f6e7a2420d1556820f58fb8b4390cdf6859
Peter <lawsonpd@gmail.com>
2017-12-03 20:03:59 -0600

    Add notify_offerer function in utilities.py.

--- tradefood/utilities.py
+++ tradefood/utilities.py
@@ -1,5 +1,22 @@
 from django.utils.timezone import now, timedelta
 
+from twilio.rest import Client
+from twofortwo.settings import TWILIO_SID, TWILIO_AUTH_TOKEN
+
+# check whether offer or bid is still alive
 def is_alive(obj):
   time_now = now()
   return obj.date_posted + timedelta(obj.duration) > time_now
+
+# send notification to offerer
+def notify_offerer(offer, test=False):
+  offer_url = 'http://pepperclove.club/offers/{0}'.format(offer.pk)
+  client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+  message = client.message.create(
+    offer.contact_phone,
+    body="Someone has bid on your offer! Visit {0} for details.".format(offer_url),
+    from_="+16156100586")
+
+# send notification to bidder
+def notify_bidder(bid):
+  client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
