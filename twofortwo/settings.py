55068ac5332d0c53510dedb6de8457832f55739b
Peter Lawson <lawsonpd@pm.me>
2020-11-17 15:22:15 -0600

    Ignore Procfile.test. Comment out whitenoise config in prod settings.

--- twofortwo/settings.py
+++ twofortwo/settings.py
@@ -163,7 +163,7 @@ STATICFILES_DIRS = (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # AUTH_USER_MODEL = 'tradefood.User'
